41c28a9029c0d621f3ae98692819466397335417
Konstantin Semenov <jhvhs@users.noreply.github.com>
2018-04-04 20:13:42 +0100

    Update trilogy.rb

--- trilogy.rb
+++ trilogy.rb
@@ -4,7 +4,7 @@ class Trilogy < Formula
   homepage 'https://github.com/pivotal/trilogy'
   version "0.3.1"
   url "https://github.com/pivotal/trilogy/releases/download/v#{version}/trilogy-#{version}-mac-linux.tgz"
-  sha256 "06658f019e660a93d6b5eb1683d8606966577ed214520a39c49bdb1e9b320e98"
+  sha256 "571bc3ab419bb295c9072fa8be2ec731edf6c3f292a011885fb45fdac9d53e46"
 
   depends_on :java => "1.8"
 
@@ -14,6 +14,6 @@ class Trilogy < Formula
   end
 
   test do
-    print "nothing"
+    system "#{bin}/trilogy --help"
   end
 end
